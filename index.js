afab7add29b2ee56102d1eedbb92c52f286f6d38
SokhibjonDev <105203720+SokhibjonDev@users.noreply.github.com>
2022-05-24 17:59:32 +0500

    carParking
    carParking com

---
+++ index.js
@@ -0,0 +1,40 @@
+const ent = document.querySelector('#ent');
+const exit = document.querySelector('#exit');
+const result = document.querySelector('.result');
+
+let entTime;
+let exitTime;
+
+ent.addEventListener('click', () => {
+
+
+    entTime = new Date()
+
+    alert(`Moshina soat ${entTime.getHours()} : ${entTime.getMinutes()} da avtoturargohgakirdi`)
+
+    exit.disabled = false
+
+    return entTime
+})
+
+exit.addEventListener('click', () => {
+    exit.disabled = true
+    ent.disabled = false
+
+    exitTime = new Date()
+
+    let res = Math.abs((exitTime.getHours() * 60 + exitTime.getMinutes()) - (entTime.getHours() * 60 + entTime.getMinutes()))
+    let hour = Math.floor(res / 60)
+    let minut = res % 60
+
+    if (minut > 0) {
+        hour = hour + 1
+    }
+
+    let money = (hour * 4) + 1
+    let span = `
+    Sizdan avtoturargohda turganigiz uchun <span>${money}$</span> boldi.
+    `
+
+    result.innerHTML = span
+})
